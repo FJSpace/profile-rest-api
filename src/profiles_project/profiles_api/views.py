ea7928b537e6f038a794abf8eb8ca817a19c08c6
Fredrik Johansson <frejohi@student.chalmers.se>
2018-08-07 09:32:08 +0200

    Added profile listing and creating feature

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -7,6 +7,13 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from . import serializers
+from . import models
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
 
 class HelloApiView(APIView):
     """Test API View."""
